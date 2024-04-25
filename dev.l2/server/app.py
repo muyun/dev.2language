79dfdb1b17b2bc5b471cb4ab82801b24aadd3a22
ZHAO WENLONG <zhaowenlong@Raymonds-MacBook.local>
2024-04-25 20:11:32 +0800

    update

--- dev.l2/server/app.py
+++ dev.l2/server/app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request, jsonify
 
 import llm
 
-def get_conversation(prompt, model=llm.get_model("orca-mini-3b-gguf2-q4_0")):
-    
+def get_conversation(
+        prompt, 
+        model=llm.get_model("orca-mini-3b-gguf2-q4_0"),
+        system = "Answer like a very friendly AI agent to provide youth emotional support",
+        ):
     conv = model.conversation()
-    response = conv.prompt(prompt)
+    response = conv.prompt(prompt, system=system)
     return response.text()
 
 # Initializing flask app

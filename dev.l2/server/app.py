e46677e79ed4abdf787e1176f4eb3c31c9f45cbb
ZHAO WENLONG <zhaowenlong@Raymonds-MacBook.local>
2024-04-26 13:32:02 +0800

    update

--- dev.l2/server/app.py
+++ dev.l2/server/app.py
@@ -1,15 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 
-import llm
 
-def get_conversation(
-        prompt, 
-        model=llm.get_model("orca-mini-3b-gguf2-q4_0"),
-        system = "Answer like a very friendly AI agent to provide youth emotional support",
-        ):
-    conv = model.conversation()
-    response = conv.prompt(prompt, system=system)
-    return response.text()
+from chat import get_conversation
+
 
 # Initializing flask app
 app = Flask(__name__)
--- dev.l2/server/chat.py
+++ dev.l2/server/chat.py
@@ -1,8 +1,12 @@
-import pprint
 import llm
 
+def get_conversation(
+        prompt, 
+        model=llm.get_model("orca-mini-3b-gguf2-q4_0"),
+        system = "Answer like a very friendly AI agent to provide youth emotional support regarding their emotions"
+        ):
+    
+    conv = model.conversation()
+    response = conv.prompt(prompt, system=system)
+    return response.text()
 
-model = llm.get_model("orca-mini-3b-gguf2-q4_0")
-conv = model.conversation()
-response = conv.prompt("Five fun facts about pelicans")
-pprint(response.text())
